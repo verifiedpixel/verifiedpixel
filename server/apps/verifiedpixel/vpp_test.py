9824f24d096e7fb811bd84ad12325468b87d17b4
actionless <actionless.loveless@gmail.com>
2015-08-03 20:32:15 +0200

    wip(server: apps: vpp): zip verified items

--- server/apps/verifiedpixel/vpp_test.py
+++ server/apps/verifiedpixel/vpp_test.py
@@ -186,3 +186,47 @@ class VerifiedPixelAppTest(TestCase):
                 self.verification_result['gris'],
                 list(items)[0]['verification']['gris']
             )
+
+    @activate_izitru_mock(
+        {"response_file": './test/vpp/test1_izitru_response.json'},
+        {"response_file": './test/vpp/test2_izitru_response.json'}
+    )
+    @activate_tineye_mock(
+        {"response_file": './test/vpp/test1_tineye_response.json'},
+        {"response_file": './test/vpp/test2_tineye_response.json'}
+    )
+    @activate_gris_mock(
+        {"response_file": './test/vpp/gris_discovery_response.json'},
+        {"response_file": './test/vpp/test1_gris_search_response.json'},
+        {"response_file": './test/vpp/gris_discovery_response.json'},
+        {"response_file": './test/vpp/test2_gris_search_response.json'}
+    )
+    def test_zip_output(self):
+        self.upload_fixture_image(
+            './test/vpp/test.png',
+            './test/vpp/test1_verification_result.json'
+        )
+        self.upload_fixture_image(
+            './test/vpp/test2.jpg',
+            './test/vpp/test2_verification_result.json'
+        )
+        with self.app.app_context():
+            test_client = app.test_client()
+            verify_ingest()
+            lookup = {'type': 'picture'}
+            items = superdesk.get_resource_service('archive').get(
+                req=ParsedRequest(), lookup=lookup
+            )
+            self.assertEqual(len(list(items)), 2)
+            item_ids = [item['_id'] for item in items]
+            vppzip_service = get_resource_service('verifiedpixel_zip')
+            zip_id = vppzip_service.post([{'items': item_ids}])[0]
+            print(zip_id)
+            zip_item = list(vppzip_service.get_from_mongo(
+                req=ParsedRequest(), lookup={"_id": zip_id}
+            ))[0]
+            pprint(zip_item)
+            response = test_client.get(zip_item['result'])
+            # @TODO: finish the test
+            with open('./test/vpp/output.zip', 'wb') as f:
+                f.write(response.get_data())
