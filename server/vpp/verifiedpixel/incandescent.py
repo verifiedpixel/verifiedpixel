1c2864e02ba3afe377c33d4b45deff70ef7a699b
actionless <actionless.loveless@gmail.com>
2015-08-21 14:07:38 +0200

    feat(server: apps: vpp: incandescent): retrieve keys from settings

--- server/vpp/verifiedpixel/incandescent.py
+++ server/vpp/verifiedpixel/incandescent.py
@@ -6,40 +6,44 @@ import time
 import datetime
 import calendar
 import json
-import urllib.request, urllib.parse, urllib.error
+import urllib.request
+import urllib.parse
+import urllib.error
 from gridfs import GridFS
 from eve.utils import ParsedRequest
 from requests import request
 from PIL import Image
 from io import BytesIO
 from hashlib import sha1 as sha
-
-from .exceptions import APIGracefulException
+from flask import current_app as app
 
 import superdesk
 
+from .exceptions import APIGracefulException
 
 # @TODO: for debug purpose
 from pprint import pprint  # noqa
 
 
 def get_incandescent_results(href):
-    uid = 1234
-    apiKey = 'secret'
+    uid = app.config['INCANDESCENT_UID']
+    apiKey = app.config['INCANDESCENT_APIKEY']
     expires = datetime.datetime.now() - datetime.timedelta(minutes=110)
     utc_expires = calendar.timegm(expires.timetuple())
     to_string = str(uid) + "\n" + str(utc_expires)
     binary_signature = hmac.new(apiKey.encode(), to_string.encode(), sha)
-    signature = urllib.parse.quote_plus(base64.b64encode(binary_signature.digest()))
+    signature = urllib.parse.quote_plus(
+        base64.b64encode(binary_signature.digest()))
     images = [href]
     add_data = {
         'uid': uid,
         'expires': utc_expires,
         'signature': signature,
-        'images': images 
+        'images': images
     }
     add_headers = {'Content-type': 'application/json'}
-    add_response = request('POST', 'https://incandescent.xyz/api/add/', data=json.dumps(add_data), headers=add_headers)
+    add_response = request(
+        'POST', 'https://incandescent.xyz/api/add/', data=json.dumps(add_data), headers=add_headers)
     if add_response.status_code != 200:
         raise APIGracefulException(add_response)
 
@@ -51,19 +55,21 @@ def get_incandescent_results(href):
         'uid': uid,
         'expires': utc_expires,
         'signature': signature,
-        'project_id': add_result['project_id'] 
+        'project_id': add_result['project_id']
     }
     get_headers = {'Content-type': 'application/json'}
     results = None
     while not results:
-        get_response = request('POST', 'https://incandescent.xyz/api/get/', data=json.dumps(get_data), headers=get_headers)
+        get_response = request(
+            'POST', 'https://incandescent.xyz/api/get/', data=json.dumps(get_data), headers=get_headers)
         get_result = get_response.json()
         if get_result.get('status') == 710:
             results = None
         else:
             results = get_result
-     
+
     return results
 
-results = get_incandescent_results('http://vppmaster.dev.superdesk.org/api/upload/55c9fdfb6cdb31004e08aa5f/raw?_schema=http')
+results = get_incandescent_results(
+    'http://vppmaster.dev.superdesk.org/api/upload/55c9fdfb6cdb31004e08aa5f/raw?_schema=http')
 print(results)
